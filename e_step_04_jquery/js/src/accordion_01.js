1251cd96eb71c3bb1475ceed58af779862e8df4c
sarahahn <yr.ahn1205@gmail.com>
2017-08-04 11:03:53 +0900

    세로형 슬라이드

---
+++ e_step_04_jquery/js/src/accordion_01.js
@@ -0,0 +1,35 @@
+// accordion_01.js
+
+(function($){
+  //==============================================
+    /* jquery 아코디언
+    *  1. 선택 개체 숨기기( hide() )
+    *  2. 선택 되지 않은 다른 개체 사라지게 만들기
+    *    2-1. 현재값에서 높이가 0이 되도록 animate
+    *    2-2. 0으로 만들어지면 hide()
+    *  3. 선택 개체 바로 뒤 요소 보이게
+    *    3-1. show()  -> height:0
+         3-2. 원래 가져야 하는 높이 값으로 설정
+    */
+
+  // $('.add_h').find('dd').hide();
+  $('.add_h').find('dd').css({display:'block', padding:0, height:0, overflow:'hidden'});
+  $('dt').on('click',function(e){
+    e.preventDefault();
+    // $('dd').hide();
+    $(this).next('dd')
+           .css({'height':'auto', 'padding':'0.5em'});
+           });
+
+
+    // callback형식이 계속 늘어나는 형태
+    //  선택자.메소드(기능, 함수(){
+    //       선택.메소드(기능, 함수(){
+    //       선택. 메소드(기능, 함수(){
+    //         ....
+    //         });
+    //     });
+    // });
+
+  //==============================================
+})(this.jQuery);
